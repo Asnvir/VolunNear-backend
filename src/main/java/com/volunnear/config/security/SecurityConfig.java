999044d4ad79ca3f18844f77338c8a1fcd9e7dd3
vlady98ish <vlady98ish@gmail.com>
2024-07-24 23:27:23 +0300

    fix config

--- src/main/java/com/volunnear/config/security/SecurityConfig.java
+++ src/main/java/com/volunnear/config/security/SecurityConfig.java
@@ -68,7 +68,6 @@ public class SecurityConfig {
                                 Routes.POST_FEEDBACK_ABOUT_ORGANISATION,
                                 Routes.UPDATE_FEEDBACK_FOR_CURRENT_ORGANISATION,
                                 Routes.DELETE_FEEDBACK_ABOUT_ORGANISATION,
-                                Routes.GET_ALL_ACTIVITIES_NAMES,
                                 Routes.GET_ACTIVITIES,
                                 Routes.SET_VOLUNTEERS_PREFERENCES,
                                 Routes.LOCATION + "/**",
